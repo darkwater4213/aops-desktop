a6917442b6001ae5b9eafd8d42aff5f0bdb1029a
darkwater4213 <53630002+darkwater4213@users.noreply.github.com>
2021-10-06 13:21:14 -0700

    Adding Chrome App (1 of 4)

---
+++ config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "AoPS",
+   "homepage": "https://artofproblemsolving.com/community/my-aops",
+   "enableHomeBttn": false,
+   "enableLogoutBttn": false,
+   "termsOfService": "WARNING: This app is NOT affiliated wit AoPS, inc., and is maintained solely by AoPS users. We CANNOT be responsible for any loss of data or damage to your computer, or compromised info. While AoPS's security protocols are rock-solid, We also cannot guarantee the security of this app. By using this app you agree to indemnify, hold harmless, and in general no sue the developers of this app for anything caused by it to your computer or any dat associated with it, AoPS account or any data associated with it, or any information accessed through this app. If you run into a problem, please notify us immediately, after confirming that it is a problem with the app, and not the site. Thank you for supporting us!",
+   "rotation": "0",
+   "kioskEnabled": false
+};
